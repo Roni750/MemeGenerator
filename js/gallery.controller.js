a1a8192ef97f1344afb7406987fbfa05c5ef15f7
Roni750 <104915367+Roni750@users.noreply.github.com>
2023-04-27 13:53:04 +0300

    Update gallery.controller.js
    Removed backslash from paths

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -8,7 +8,7 @@ function onInit() {
 function renderGallery() {
     let strHtml = ``
     gImgs.map(img => {
-        strHtml += `<img src="/img/square/${img.id}.jpg" onclick="onSelectTemplate(${img.id})">`
+        strHtml += `<img src="img/square/${img.id}.jpg" onclick="onSelectTemplate(${img.id})">`
         // strHtml += `<img src="img/square/${img.id}.jpg" onclick="renderImg(${img.id})">`
     })
     let elGallery = document.querySelector('.gallery-container')
